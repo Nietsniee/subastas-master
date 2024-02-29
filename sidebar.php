f9f788cf9b7ffc04dbb013afd3d50749a0cb1275
Nietsniee <nijovesa@gmail.com>
2024-02-29 08:29:52 +0100

    Se agrega el proyecto

---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="alta_subasta.php"><i class="fa fa-fw fa-plus"></i> Nueva subasta</a>
+        </li>
+        
+    </ul>
+</div>
